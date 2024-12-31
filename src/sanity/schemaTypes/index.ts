a3e7afaa0b1298fdd686a3a79b6b168a793928af
YourGitHubUsername <armeennadeem197>
2024-12-31 15:48:29 +0500

    Updated sanity client configuration and fixed import paths

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,8 +1,8 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import products from './products'
-import category from './category'
-import heroImages from './heroImages'
-
+import { type SchemaTypeDefinition } from 'sanity';
+import products from './products';
+import category from './category';
+import heroImages from './heroImages';
+// Defining the schema with all types
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [products,category,heroImages],
-}
+  types: [products, category, heroImages],
+};
